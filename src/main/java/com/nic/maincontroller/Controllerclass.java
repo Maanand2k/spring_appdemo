d4951154fe1d364eed56406d600922fd267827f2
Anand <maanand2k@gmail.com>
2022-11-09 08:38:09 +0530

    added Update Service

--- src/main/java/com/nic/maincontroller/Controllerclass.java
+++ src/main/java/com/nic/maincontroller/Controllerclass.java
@@ -2,6 +2,7 @@ package com.nic.maincontroller;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,43 +18,65 @@ import com.nic.service.Serviceclass;
 
 @RestController
 public class Controllerclass {
-	
+
+	private Serviceclass serviceclass;
+
 	@Autowired
-	private Serviceclass s;
-	
+	public void Controllerclass(Serviceclass serviceclass) {
+		this.serviceclass = serviceclass;
+	}
+
 	@GetMapping("/get")
-	public ResponseEntity<List<modelclass>> getdetails()
-	{
-		List<modelclass> getusers = s.getform();
+	public ResponseEntity<List<modelclass>> getdetails() {
+		List<modelclass> getusers = serviceclass.getform();
 		return ResponseEntity.ok(getusers);
 	}
-	
+
 	@PostMapping("/set")
-	public ResponseEntity<modelclass> setdetails(modelclass m) throws URISyntaxException
-	{
-		modelclass newuser = (modelclass) s.saveform(m);
-		return ResponseEntity.created(new URI("/set"+ newuser.getId())).body(newuser);
+	public ResponseEntity<modelclass> setdetails(modelclass m) throws URISyntaxException {
+		modelclass newuser = (modelclass) serviceclass.saveform(m);
+		return ResponseEntity.created(new URI("/set" + newuser.getId())).body(newuser);
 	}
-	
+
 	@GetMapping("/getbyfirstname")
-	public ResponseEntity<List<modelclass>> getbyfirstname(@RequestParam String name)
-	{
-		List<modelclass> userfirstname = s.findbyfirstname(name);
+	public ResponseEntity<List<modelclass>> getbyfirstname(@RequestParam String name) {
+		List<modelclass> userfirstname = serviceclass.findbyfirstname(name);
 		return ResponseEntity.ok(userfirstname);
 	}
-	
+
 	@GetMapping("/getbyage")
-	public ResponseEntity<List<modelclass>> getbyage(@RequestParam int age)
-	{
-		List<modelclass> userage = s.findbyage(age);
+	public ResponseEntity<List<modelclass>> getbyage(@RequestParam int age) {
+		List<modelclass> userage = serviceclass.findbyage(age);
 		return ResponseEntity.ok(userage);
 	}
-	
-//	@PutMapping("/updatetable")
-//	public ResponseEntity<modelclass> updation(modelclass m) throws URISyntaxException
-//	{
-//		modelclass updateuser = (modelclass) s.saveform(m);
-//		return ResponseEntity.ok(new URI("/updatetable"+ updateuser.getId())).body(updateuser);
-//	}
-	
+
+	@PutMapping("/update")
+	public void updateuser(modelclass m) {
+		serviceclass.updateuser(m);
+	}
+
+	// @PostMapping("/getjwt")
+	// public List<Map<String,Object>> jwttoken(@RequestParam String
+	// subject,@RequestParam String role)
+	// {
+	// List<Map<String,Object>> listvalue = new ArrayList<>();
+	// String jwtString;
+	// Map<String,Object> mapvalue = new HashMap<>();
+	// jwtString = serviceclass.createJWTToken(subject, role);
+	// mapvalue.put("JwtToken", jwtString);
+	// listvalue.add(mapvalue);
+
+	// return listvalue;
+
+	// }
+
+	// @PutMapping("/updatetable")
+	// public ResponseEntity<modelclass> updation(modelclass m) throws
+	// URISyntaxException
+	// {
+	// modelclass updateuser = (modelclass) s.saveform(m);
+	// return ResponseEntity.ok(new URI("/updatetable"+
+	// updateuser.getId())).body(updateuser);
+	// }
+
 }
--- src/main/java/com/nic/service/Serviceclass.java
+++ src/main/java/com/nic/service/Serviceclass.java
@@ -1,7 +1,9 @@
 package com.nic.service;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.nic.model.modelclass;
@@ -9,30 +11,40 @@ import com.nic.repository.Repositoryinterface;
 
 @Service
 public class Serviceclass {
-	
+
+	@Value("${token.key}")
+	static String key;
+
+	// public static SecretKey SHARED_SECRET = new
+	// SecretKeySpec(Base64.getDecoder().decode(key),"HmacSHA256");
+
 	@Autowired
 	public Repositoryinterface r;
-	
+
 	public List<modelclass> getform() {
 		return r.findAll();
 	}
-	
-	public modelclass saveform(modelclass m)
-	{
+
+	public modelclass saveform(modelclass m) {
 		return r.save(m);
 	}
 
-	public List<modelclass> findbyfirstname(String name)
-	{
+	public List<modelclass> findbyfirstname(String name) {
 		List<modelclass> demolist = r.findByFirstname(name);
 		return demolist;
 	}
-	
-	public List<modelclass> findbyage(Integer age)
-	{
+
+	public List<modelclass> findbyage(Integer age) {
 		List<modelclass> demolist = r.findByAgeGreaterThan(age);
 		return demolist;
-	} 
-	
-	
+	}
+
+	public void updateuser(modelclass model) {
+		r.findById(model.getId()).ifPresent(t -> {
+			t.setAge(model.getAge());
+			t.setFirstname(model.getFirstname());
+			t.setLastname(model.getLastname());
+			r.save(t);
+		});
+	}
 }
